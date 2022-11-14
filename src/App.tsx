ced513acf9f6c8975beb07db2867b264aa3b229b
Steven Nguyen <stevenn38@hotmail.com>
2022-11-14 17:13:01 +1300

    Move and reconfigure global css

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./App.css";
 import Home from "@pages/Home";
 import About from "@pages/About";
 
 function App() {
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/about" element={<About />} />
-			</Routes>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,9 @@
 const Home = () => {
-  return (
-    <div className="flex h-screen w-full items-center justify-center bg-black text-white">
-      Home
-    </div>
-  );
+    return (
+        <div className="flex h-screen w-full items-center justify-center bg-black text-white">
+            <h1 className="heading-1">Home</h1>
+        </div>
+    );
 };
 
 export default Home;
