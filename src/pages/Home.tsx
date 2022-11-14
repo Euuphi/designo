17845035229f0e9d78361b3a7805d82f92b5c023
Steven Nguyen <stevenn38@hotmail.com>
2022-11-14 17:31:30 +1300

    Set base typography

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 const Home = () => {
     return (
         <div className="flex h-screen w-full items-center justify-center bg-black text-white">
-            <h1 className="heading-1">Home</h1>
+            <h1 className="heading-2">Home</h1>
         </div>
     );
 };
