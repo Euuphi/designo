9599e939e00bebbe479825c00c93e0f2ae2d8e10
Steven Nguyen <stevenn38@hotmail.com>
2022-11-15 16:16:07 +1300

    Style footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,3 +1,5 @@
+import useMediaQuery, { screens } from "@/hooks/userMediaQuery";
+// Assets
 import logoLight from "assets/shared/desktop/logo-light.png";
 import { ReactComponent as FacebookLogo } from "assets/shared/desktop/icon-facebook.svg";
 import { ReactComponent as YoutubeLogo } from "assets/shared/desktop/icon-youtube.svg";
@@ -5,27 +7,33 @@ import { ReactComponent as TwitterLogo } from "assets/shared/desktop/icon-twitte
 import { ReactComponent as PinterestLogo } from "assets/shared/desktop/icon-pinterest.svg";
 import { ReactComponent as InstagramLogo } from "assets/shared/desktop/icon-instagram.svg";
 // Components
+import { NavLink } from "react-router-dom";
 import FooterNav from "./FooterNav";
 
 const Footer = () => {
+    const md = useMediaQuery(screens.md);
+
     return (
-        <footer className="bg-black py-16 text-[#8e8e8f]">
-            <div className="m-auto w-11/12">
-                <div className="flex flex-col items-center md:flex-row">
-                    <img
-                        className="w-52 py-8"
-                        src={logoLight}
-                        alt="Designo logo"
-                    />
+        <footer className="outer-container bg-black pb-16 pt-16 text-[#8e8e8f] md:py-20">
+            <div className="m-auto">
+                <div className="flex flex-col items-center py-2 md:mb-8 md:flex-row md:justify-between md:border-b md:border-grey-dark md:pb-10">
+                    <NavLink to="/">
+                        <img
+                            className="w-52 pb-8 md:pb-0"
+                            src={logoLight}
+                            alt="Designo logo"
+                        />
+                    </NavLink>
+                    {!md && <hr className="w-full border-t border-grey-dark" />}
                     <FooterNav />
                 </div>
-                <div className="flex flex-col items-center gap-12">
-                    <address className="space-y-2 text-center not-italic">
+                <div className="flex flex-col items-center gap-12 md:flex-row md:gap-16 lg:gap-44 xl:gap-52">
+                    <address className="space-y-2 text-center not-italic md:text-left">
                         <h1 className="font-bold">Designo Central Office</h1>
                         <p>3886 Wellington Street</p>
                         <p>Toronto, Ontario M9C 3J5</p>
                     </address>
-                    <div className="space-y-1 text-center">
+                    <div className="space-y-1 text-center md:text-left">
                         <h1 className="font-bold">
                             Contact Us (Central Office)
                         </h1>
@@ -48,7 +56,7 @@ const Footer = () => {
                             </a>
                         </p>
                     </div>
-                    <div className="flex gap-4">
+                    <div className="flex gap-4 md:ml-auto md:self-end">
                         <FacebookLogo className="social-icon" />
                         <YoutubeLogo className="social-icon" />
                         <TwitterLogo className="social-icon" />
