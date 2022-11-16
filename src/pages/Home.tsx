28dc541c5e0f6042777b1e0e481dd3b07de213ff
Steven Nguyen <stevenn38@hotmail.com>
2022-11-16 16:53:27 +1300

    Home page hero section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,35 @@
+import useMediaQuery, { screens } from "@/hooks/userMediaQuery";
+import heroImage from "@/assets/home/desktop/image-hero-phone.png";
 import Button from "@/components/Button";
 
 const Home = () => {
+    const sm = useMediaQuery(screens.sm);
+
     return (
-        <>
-            <div className="flex h-screen w-full items-center justify-center gap-5 bg-peach text-white">
-                <h1 className="heading-2">Home</h1>
-                <Button text="Learn More" />
-            </div>
-        </>
+        <div className="mb-96 sm:container">
+            <main className="flex flex-col items-center overflow-hidden bg-peach px-6 pt-20 text-white sm:rounded-2xl">
+                <div className="flex max-w-[21rem] flex-col items-center gap-6 text-center md:max-w-[30rem] md:gap-8">
+                    <h1 className="heading-1">
+                        Award-winning custom designs and digital branding
+                        solutions
+                    </h1>
+                    <p>
+                        With over 10 years in the industry, we are experienced
+                        in creating fully responsive websites, app design, and
+                        engaging brand experiences. Find out more about our
+                        services.
+                    </p>
+                    <Button text="Learn More" />
+                </div>
+                <img
+                    className="max-w-screen w-80 translate-y-[30%] scale-[2]"
+                    src={heroImage}
+                    alt="Iphone displaying vase image and word 'Frame'"
+                />
+            </main>
+            <section className="px-6 sm:px-0">Cards</section>
+            <section className="px-6 sm:px-0">Illustrations</section>
+        </div>
     );
 };
 
