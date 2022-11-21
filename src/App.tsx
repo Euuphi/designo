1907978a7bc526d4042a21a2ae6d89cccb8c409a
Steven Nguyen <stevenn38@hotmail.com>
2022-11-22 00:34:16 +1300

    Add service page routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "@/components/Header/Header";
+import Footer from "@/components/Footer/Footer";
+// Pages
 import Home from "@/pages/Home";
 import About from "@/pages/About";
 import Locations from "@/pages/Locations";
 import Contact from "@/pages/Contact";
-import Header from "@/components/Header/Header";
-import Footer from "@/components/Footer/Footer";
+import WebDesign from "@/pages/Services/WebDesign";
+import AppDesign from "./pages/Services/AppDesign";
+import GraphicDesign from "./pages/Services/GraphicDesign";
 
 function App() {
     return (
@@ -13,6 +17,14 @@ function App() {
                 <Header />
                 <Routes>
                     <Route path="/" element={<Home />} />
+                    <Route path="/services">
+                        <Route path="web-design" element={<WebDesign />} />
+                        <Route path="app-design" element={<AppDesign />} />
+                        <Route
+                            path="graphic-design"
+                            element={<GraphicDesign />}
+                        />
+                    </Route>
                     <Route path="/about" element={<About />} />
                     <Route path="/locations" element={<Locations />} />
                     <Route path="/contact" element={<Contact />} />
