b2e68fadf2f138dd69ed76c7dcf2d41022fe345a
Steven Nguyen <stevenn38@hotmail.com>
2022-12-26 23:16:11 +1300

    Scretch service page hero section on mobile

--- src/sections/ServiceHero.tsx
+++ src/sections/ServiceHero.tsx
@@ -8,11 +8,11 @@ interface Props {
 const ServiceHero = ({ title, description }: Props) => {
     return (
         <section
-            className="flex flex-col items-center justify-center gap-8 bg-peach px-8 py-24 text-center text-white md:py-16"
+            className="-mx-40 flex flex-col items-center justify-center gap-8 bg-peach bg-[top_right] bg-no-repeat py-24 px-8 text-center text-white sm:mx-0 sm:rounded-2xl md:py-16 lg:bg-[center_right]"
             style={{ backgroundImage: `url('${bgImage}')` }}
         >
             <h1 className="heading-1">{title}</h1>
-            <p>{description}</p>
+            <p className="max-w-md">{description}</p>
         </section>
     );
 };
