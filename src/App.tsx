04581119e6539862c13ecf98c04e34949d152fd5
Steven Nguyen <stevenn38@hotmail.com>
2022-12-27 12:58:20 +1300

    Refactor: React Router to v6

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import ScrollToTop from "./utility/ScrollToTop";
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
-// Pages
-import Home from "@/pages/Home";
-import About from "@/pages/About";
-import Locations from "@/pages/Locations";
-import Contact from "@/pages/Contact";
-import WebDesign from "@/pages/Services/WebDesign";
-import AppDesign from "./pages/Services/AppDesign";
-import GraphicDesign from "./pages/Services/GraphicDesign";
 
 function App() {
     return (
         <>
-            <BrowserRouter>
-                <ScrollToTop />
-                <Header />
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/services">
-                        <Route path="web-design" element={<WebDesign />} />
-                        <Route path="app-design" element={<AppDesign />} />
-                        <Route
-                            path="graphic-design"
-                            element={<GraphicDesign />}
-                        />
-                    </Route>
-                    <Route path="/about" element={<About />} />
-                    <Route path="/locations" element={<Locations />} />
-                    <Route path="/contact" element={<Contact />} />
-                </Routes>
-                <Footer />
-            </BrowserRouter>
+            <ScrollToTop />
+            <Header />
+            <Outlet />
+            <Footer />
         </>
     );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,63 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+// React Router
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// Components
 import App from "./App";
 import "@/styles/index.css";
+// Pages
+import Home from "@/pages/Home";
+import About from "@/pages/About";
+import Locations from "@/pages/Locations";
+import Contact from "@/pages/Contact";
+import WebDesign from "@/pages/Services/WebDesign";
+import AppDesign from "./pages/Services/AppDesign";
+import GraphicDesign from "./pages/Services/GraphicDesign";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        children: [
+            {
+                path: "/",
+                element: <Home />,
+            },
+            {
+                path: "services",
+                children: [
+                    {
+                        path: "web-design",
+                        element: <WebDesign />,
+                    },
+                    {
+                        path: "app-design",
+                        element: <AppDesign />,
+                    },
+                    {
+                        path: "graphic-design",
+                        element: <GraphicDesign />,
+                    },
+                ],
+            },
+            {
+                path: "about",
+                element: <About />,
+            },
+            {
+                path: "locations",
+                element: <Locations />,
+            },
+            {
+                path: "contact",
+                element: <Contact />,
+            },
+        ],
+    },
+]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
-        <App />
+        <RouterProvider router={router} />
     </React.StrictMode>
 );
