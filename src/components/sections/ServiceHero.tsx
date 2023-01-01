05cd866f4c263f68b47e672389370094a8af47bd
Steven Nguyen <stevenn38@hotmail.com>
2023-01-01 23:34:51 +1300

    Add class for section spacing

--- src/components/sections/ServiceHero.tsx
+++ src/components/sections/ServiceHero.tsx
@@ -6,7 +6,7 @@ interface Props {
 const ServiceHero = ({ title, description }: Props) => {
 	return (
 		<section
-			className="flex flex-col items-center justify-center gap-8 bg-peach bg-[top_right] bg-no-repeat py-24 text-center text-white sm-max:-mx-40 sm-max:px-40 sm:mx-0 sm:rounded-2xl md:py-16 lg:bg-[center_right]"
+			className="section-space-b flex flex-col items-center justify-center gap-8 bg-peach bg-[top_right] bg-no-repeat py-24 text-center text-white sm-max:-mx-40 sm-max:px-40 sm:mx-0 sm:rounded-2xl md:py-16 lg:bg-[center_right]"
 			style={{
 				backgroundImage:
 					"url('/assets/web-design/desktop/bg-pattern-intro-web.svg')",
