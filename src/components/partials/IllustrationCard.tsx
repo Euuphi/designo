739f6bbed0d1bd646381f44f1cb0f87aa74afcaf
Steven Nguyen <stevenn38@hotmail.com>
2023-01-05 12:16:54 +1300

    Fix: Illustration card component icon not showing

--- src/components/partials/IllustrationCard.tsx
+++ src/components/partials/IllustrationCard.tsx
@@ -1,12 +1,23 @@
+import React from "react";
+
 interface Props {
-	icon: SVGRectElement;
+	icon: React.ElementType;
 	title: string;
 	text: string;
 }
 
-const IllustrationCard = ({ icon, title, text }: Props) => {
+/**
+ *
+ *
+ * @param {React.ElementType} icon - Icon as a React Component/Element
+ * @param {string} title - Title of card
+ * @param {string} text - Body text of card
+ * @return {*}
+ */
+const IllustrationCard = ({ icon: Icon, title, text }: Props) => {
 	return (
 		<div className="flex flex-col items-center justify-center gap-12 text-center md:flex-row md:text-left lg:flex-col lg:text-center">
+			<Icon />
 			<div>
 				<h2 className="heading-3 mb-8 uppercase md:mb-4 lg:mb-8">
 					{title}
